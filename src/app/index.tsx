1041da5ed47b3f8580867811398929f3cddcca36
Alex Rofes <alexrofes1992@gmail.com>
2024-05-07 12:47:00 +0200

    nav

---
+++ src/app/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "@/Components/Nav";
+import 'tailwindcss/tailwind.css';
+
+
+const HomePage = () => {
+  return <div className="overflow-x-hidden">
+  <div>
+    <Nav />
+  </div>
+  </div>
+}; 
+
+export default HomePage;
